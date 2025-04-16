6069d7cef58ae092bb2c65f8e7199629ddc8f91f
danielll23 <danielcp2334@gmail.com>
2025-04-16 10:07:59 +0700

    nambahin dan ganti logo sama nambahin table dari spatie

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,6 +2,14 @@ import { HTMLAttributes } from 'react';
 
 export default function AppLogoIcon(props: HTMLAttributes<HTMLImageElement>) {
     return (
-        <img src="/logo.svg" alt="Joshua Generation Logo" {...props} />
+        <div className="relative h-8 w-8"> {/* Increase size if needed */}
+            <img
+                src="/logo/logolight.png"
+                alt="Logo Light"
+                className="absolute inset-0 h-full w-full object-contain block dark:hidden"
+                {...props}
+            />
+    
+        </div>
     );
 }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -33,7 +33,8 @@
         <title inertia>{{ config('app.name', 'Joshua Generation') }}</title>
         
         <!-- Add favicon -->
-        <link rel="icon" href="/logo.svg" type="image/svg+xml">
+       <link rel="icon" href="/logo/logoJG.png" type="image/png">
+
         <link rel="alternate icon" href="/favicon.ico" type="image/png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
