8edb047b05ebe3fafe8ca681dd60abc4eabc2e9f
IvanYonathan <ivanyonathan.siwu@gmail.com>
2026-01-07 12:56:40 +0700

    chore: add back DEVELOPMENT_GUIDE.md and update Upcoming VOTD.csv

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    //base: '', turn on for ngrok or subdomain hosting
+    //base: '', //turn on for ngrok or subdomain hosting
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
